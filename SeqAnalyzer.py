12bf937b92a3f29bbdf00785bfcd1a10258ffd3d
ComfyCoyote <muzzadamjee@gmail.com>
2025-02-28 14:26:41 -0800

    Modifications to SeqAnalyzer class: self.parsed_sequences is now fixed in memory instead of a generator; self.index_sequence_map maps indeces to sequences; self.id_sequence_map attempts to map id's to sequences but many sequences share id (i have no clue why the dataset utilizes non unique ids); self.chromosome_range_map maps chromosome ranges to sequences; added helper functions for each mapping for easy indexing and retrieval; introduces get_longest_common_sequence which aims to find the longest common sequence between two sequences

--- SeqAnalyzer.py
+++ SeqAnalyzer.py
@@ -1,47 +1,64 @@
 from Bio import SeqIO
 from Bio.Seq import Seq, MutableSeq
+from Bio.SeqRecord import SeqRecord
 from typing import List
+import re
 
 
 class SeqAnalyzer:
 
     def __init__(self, filepath: str, filetype: str):
-        self.parsed_sequence = SeqIO.parse(filepath, filetype)
+        self.parsed_sequence = []
+        self.index_sequence_map = {}
+        self.id_sequence_map = {}
+        self.chromosome_range_map = {}
+        for index, rec in enumerate(SeqIO.parse(filepath, filetype)):
+            match = re.search(r"range=([^\s]+)", rec.description)
+            range = match.group(1) if match else None
+            self.chromosome_range_map[range] = rec
+            self.parsed_sequence.append(rec)
+            self.index_sequence_map[index] = rec
+            self.id_sequence_map[rec.id] = rec
+            #self.range_sequence_map
 
+            
     def print_all_sequences(self) -> None:
         for seq_record in self.parsed_sequence:
             print(seq_record.id)
             print(repr(seq_record.seq))
             print(len(seq_record))
 
-    def get_sequence_by_index(self, index: int) -> Seq:
-        try:
-            found_seq = None
-            for i, seq in enumerate(self.parsed_sequence):
-                if i == index:
-                    print(f"Retrieved sequence: {seq.id}, at index: {index}")
-                    found_seq = seq
-                    break
-        except Exception as e:
-            print(f"Unable to retrieve sequence at index: {index}, error: {e}")
-        finally:
-            return found_seq
+    def get_sequence_by_index(self, index: int) -> SeqRecord:
+        seq = self.index_sequence_map.get(index, None)
 
-    def get_sequence_by_id(self, id: str) -> Seq:
-        try:
-            found_seq = None
-            for i, seq in enumerate(self.parsed_sequence):
-                if seq.id == id:
-                    print(f"Retrieved sequence: {seq.id}, at index: {i}")
-                    found_seq = seq
-                    break
-        except Exception as e:
-            print(f"Unable to retrieve sequence with id: {id}, error: {e}")
-        finally:
-            return found_seq
+        if seq:
+            print(f"Retrieved sequence: {seq.id}, at index: {index}")
+            return seq
+        else:      
+            print(f"Unable to retrieve sequence at index: {index}")
+        
+
+    def get_sequence_by_id(self, id: str) -> SeqRecord:
+        seq = self.id_sequence_map.get(id, None)
+
+        if seq:
+            print(f"Retrieved sequence: {seq.id}")
+            return seq
+        else:      
+            print(f"Unable to retrieve sequence with id: {id}")
+
+    def get_sequence_by_chromosome_range(self, range: str) -> SeqRecord:
+        seq = self.chromosome_range_map.get(range, None)
+
+        if seq:
+            print(f"Retrieved sequence: {range}")
+            return seq
+        else:      
+            print(f"Unable to retrieve sequence with range: {range}")
+
 
     # very computationally complex, On^2
-    def get_sequences_by_subsequence(self, sub: str | Seq | MutableSeq) -> List[Seq]:
+    def get_sequences_by_subsequence(self, sub: str | SeqRecord | MutableSeq) -> List[SeqRecord]:
         try:
             found_sequences = []
             for i, seq in enumerate(self.parsed_sequence):
@@ -52,4 +69,44 @@ class SeqAnalyzer:
             print(f"Unable to retrieve sequences, error: {e}")
         finally:
             return found_sequences
+        
+    def get_longest_common_sequence(self, input1: str | int | Seq, input2: str | int | Seq) -> Seq | str:
+        seq1, seq2 = None, None
+        if isinstance(input1, int):
+            seq1 = self.index_sequence_map.get(input1)
+        if isinstance(input2, int):
+            seq2 = self.index_sequence_map.get(input2)
+        if isinstance(input1, Seq):
+            seq1 = input1
+        if isinstance(input2, Seq):
+            seq2 = input2
+        if isinstance(input1, str) and 'hs1' in input1:
+            seq1 = self.id_sequence_map.get(input1)
+        if isinstance(input1, str):
+            seq1 = input1
+        if isinstance(input2, str) and 'hs1' in input2:
+            seq1 = self.id_sequence_map.get(input2)
+        if isinstance(input2, str):
+            seq1 = input2
+        #common dynamic programming logic to find longest common substring
+        m, n = len(seq1), len(seq2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]  # DP table initialized with 0
+        max_length = 0  # Stores the length of the longest common substring
+        end_index = 0   # Stores the ending index of LCS in str1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if seq1[i - 1] == seq2[j - 1]:  # Characters match
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    
+                    if dp[i][j] > max_length:  # Update max_length and end_index
+                        max_length = dp[i][j]
+                        end_index = i
+
+        # Extract the longest common substring using end_index and max_length
+        longest_subsequence = seq1[end_index - max_length:end_index]
+        print(f"Retrieved longest common subsequence: {longest_subsequence}")
+        return longest_subsequence
+
+
 
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from SeqAnalyzer import *
+from pprint import pprint
+import re
 
 analyzer = SeqAnalyzer("Jalview_U2_alignment.fa", "fasta")
 
+'''
+Some random chromosome ranges
+chr17:22821101-22821389
+chr4:86914288-86914576
+chr12:9138151-9138439
+chr3:68894838-68895126
+chr4:3177299-3177586
+chr14:74977151-74977439
+chr14:24233967-24234254
+chr11:102851103-102851391
 
+'''
 
 if __name__ == '__main__':
     #analyzer.print_all_sequences()
-    analyzer.get_sequence_by_index(0)
-    analyzer.get_sequence_by_id('hs1_t2tRepeatMasker_U2#snRNA/51-120')
-    analyzer.get_sequences_by_subsequence('ATTGCTTCTCGGCCTTTTGGCTAAGATCAAGCGTAAACCACAAACAGA')
+    #seq1 = analyzer.get_sequence_by_index(0)
+    #seq2 = analyzer.get_sequence_by_index(3)
+
+    pprint(len(analyzer.chromosome_range_map.keys()))
+
+    seq1 = analyzer.get_sequence_by_chromosome_range('chr4:86914288-86914576')
+    seq2 = analyzer.get_sequence_by_chromosome_range('chr14:74977151-74977439')
+
+    seq1_string = str(seq1.seq)
+    seq2_string = str(seq2.seq)
+
+    print(seq1_string)
+    print(seq2_string)
+
+    print(len(seq1_string))
+    print(len(seq2_string))
+    
+    #analyzer.get_sequences_by_subsequence('ATTGCTTCTCGGCCTTTTGGCTAAGATCAAGCGTAAACCACAAACAGA')
+    analyzer.get_longest_common_sequence(seq1.seq, seq2.seq)
+
+
+   
+
